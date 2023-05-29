295fa631649cd09b69ea46af861bdac4544c274b
dongheeYoo <yoodhajou0616@gmail.com>
2023-05-29 21:43:47 +0900

    Feature#2: Set up initial Layout (#3)
    * set up react router and antd
    
    * set up router at main tsx and apply
    
    * set up initial pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "./components/MainPage";
+import { Navbar } from "./components/Navbar";
+import { LoginPage } from "./components/LoginPage";
+import { SignUpPage } from "./components/SignUpPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">Start</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="app-body">
+        <Routes>
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/login"} element={<LoginPage />} />
+          <Route path={"/sign-up"} element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
